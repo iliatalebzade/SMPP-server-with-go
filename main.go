d45d86344138d55a9980dbf88722296ce207a771
Ilia Talebzadeh <ilyatalebzadeh82@gmail.com>
2023-06-29 13:26:41 +0330

    Modefied the code too look cleaner and more idiomatic

--- main.go
+++ main.go
@@ -2,27 +2,30 @@ package main
 
 import (
 	"encoding/binary"
-	"fmt"
 	"log"
 	"net"
+)
+
+const (
+	bindTransmitterCommandID = 0x00000002
 )
 
 func main() {
 	// Start the TCP server
 	listener, err := net.Listen("tcp", "0.0.0.0:2775")
 	if err != nil {
-		fmt.Println("Failed to start server:", err)
+		log.Println("Failed to start server:", err)
 		return
 	}
 	defer listener.Close()
 
-	fmt.Println("Server started. Listening on localhost:9000")
+	log.Println("Server started. Listening on 0.0.0.0:2775")
 
 	for {
 		// Accept incoming client connections
 		conn, err := listener.Accept()
 		if err != nil {
-			fmt.Println("Error accepting connection:", err)
+			log.Println("Error accepting connection:", err)
 			continue
 		}
 
@@ -34,41 +37,33 @@ func main() {
 func handleClient(conn net.Conn) {
 	defer conn.Close()
 
-	fmt.Println("New client connected:", conn.RemoteAddr())
+	log.Println("New client connected:", conn.RemoteAddr())
 
 	// Read client request from the connection
 	request := make([]byte, 1024)
 	n, err := conn.Read(request)
 	if err != nil {
-		fmt.Println("Error reading request:", err)
+		log.Println("Error reading request:", err)
 		return
 	}
 
-	fmt.Printf("Received request from client %s: %s\n", conn.RemoteAddr(), string(request[:n]))
+	log.Printf("Received request from client %s: %s\n", conn.RemoteAddr(), string(request[:n]))
 
 	// Validate the request
-	if isValidBindTransmitterRequest(request[:n]) {
+	if isValidRequest(request[:n], bindTransmitterCommandID) {
 		// Send a successful response
 		response := []byte{0x00, 0x00, 0x00, 0x10, 0x80, 0x00, 0x00, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01}
-		_, err = conn.Write(response)
-		if err != nil {
-			fmt.Println("Error sending response:", err)
-		} else {
-			fmt.Printf("Sent response to client %s\n", conn.RemoteAddr())
-		}
+		sendResponse(conn, response)
+		log.Printf("Sent response to client %s\n", conn.RemoteAddr())
 	} else {
 		// Send an error response
 		response := []byte{0x00, 0x00, 0x00, 0x10, 0x80, 0x00, 0x00, 0x02, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x01}
-		_, err = conn.Write(response)
-		if err != nil {
-			fmt.Println("Error sending response:", err)
-		} else {
-			fmt.Printf("Sent response to client %s\n", conn.RemoteAddr())
-		}
+		sendResponse(conn, response)
+		log.Printf("Sent response to client %s\n", conn.RemoteAddr())
 	}
 }
 
-func isValidBindTransmitterRequest(request []byte) bool {
+func isValidRequest(request []byte, expectedCommandID uint32) bool {
 	// Validate the request based on the SMPP protocol specifications
 	// Check the command length
 	if len(request) < 16 {
@@ -78,8 +73,8 @@ func isValidBindTransmitterRequest(request []byte) bool {
 
 	// Check the command ID
 	commandID := binary.BigEndian.Uint32(request[4:8])
-	if commandID != 0x00000002 {
-		log.Println("Invalid PDU: the command id was not valid for a bind_transmitter request")
+	if commandID != expectedCommandID {
+		log.Println("Invalid PDU: the command ID was not valid for the expected command")
 		return false
 	}
 
@@ -87,4 +82,11 @@ func isValidBindTransmitterRequest(request []byte) bool {
 
 	log.Println("Valid PDU!")
 	return true
+}
+
+func sendResponse(conn net.Conn, response []byte) {
+	_, err := conn.Write(response)
+	if err != nil {
+		log.Println("Error sending response:", err)
+	}
 }
